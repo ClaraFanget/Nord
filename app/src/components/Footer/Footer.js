15fd55721fa889613e51bfa7b7e18c825c7d7439
Clara <cfanget@ensc.fr>
2024-05-30 14:03:41 +0200

    Changement

--- app/src/components/Footer/Footer.js
+++ app/src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css";
 function Footer({ setPage }) {
   return (
     <div className="Footer">
-      <p>© NORD 2024. All Rights Reserved.</p>
+      <p>© NORD 2024. All Rights Reserved. Clara</p>
     </div>
   );
 }
